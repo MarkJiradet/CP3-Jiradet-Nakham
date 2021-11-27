64238bf0968c72bc2995a4a0b9e8d681b13517f3
MarkJiradet <jiradetn@gmail.com>
2021-11-27 21:22:24 +0700

    Create Excercise4.py

---
+++ Excercise4.py
@@ -0,0 +1,10 @@
+Foundation_English = 76.5
+General_Business = 60
+Introduction_to_ComputerSystems = 80.5
+Computer_Programming = 90.58
+
+print("--- Your Score ---")
+print("Foundation English :",Foundation_English)
+print("General Business :",General_Business)
+print("Introduction to Computer Systems :",Introduction_to_ComputerSystems)
+print("Computer Programming :",Computer_Programming)
